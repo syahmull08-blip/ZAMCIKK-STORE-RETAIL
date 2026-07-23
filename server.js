3066127f5bbb68c2b3cead673c5add9c0bb8fe60
Azam <syahmull08@gmail.com>
2026-07-23 08:49:56 +0800

    Fix PORT for Vercel

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.get("/api/products", async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server berjalan di http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
 });
